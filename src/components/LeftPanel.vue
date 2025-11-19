31a44f86db515b8d924acb62a81a7e69dafe1236
guigutan <345405123@qq.com>
2025-11-19 18:00:11 +0800

    11-19下班提交

---
+++ src/components/LeftPanel.vue
@@ -0,0 +1,67 @@
+
+<script setup lang="ts">
+    import { ref } from 'vue' 
+    const count = ref("RPA")
+</script>
+
+
+<template>
+    <div class="sc-header"><img src="/vite.svg" class="logo" /><div class="sc-title">数据查询中心</div></div>
+     <div class="sc-menu">
+        <ul  class="sc-nav">
+            <li><a href="#">条码追溯（KsMes）</a></li>
+            <li><a href="#">条码追溯（SMOM）</a></li>
+            <li><a href="#">RPA预付流程</a></li>
+            <li><a href="#">RPA到付流程</a></li>
+            <li><a href="#">{{count}}工艺路线导入</a></li>
+        </ul>
+     </div>   
+</template>
+
+
+<style scoped>
+    .sc-header{
+        margin: 0;
+        width: 100%;
+        display: flex; 
+        flex-wrap: nowrap;  
+        justify-content:center;
+        padding: 1.5em 0;       
+        border-bottom: 1px solid  #d6d4d4;
+        overflow: hidden;       
+    } 
+    .sc-header img{
+        width: 1.5em;
+        height: 1.5em;         
+        margin-right: 0.5em;       
+    }
+    .sc-header .title{
+       font-size: medium;
+       font-weight: bold;
+    }
+
+  
+    .sc-menu ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;     
+    }
+    .sc-menu ul li {
+    list-style-type: none;
+    padding-left: 0.8em; 
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    }
+    .sc-menu ul li:hover{background-color: #cad8a9;}
+
+    .sc-menu ul li a{color: #323233;}
+    .sc-menu ul li a:hover{color: #ff0202;}
+    .sc-menu ul li a:active{color: #323233;}
+    .sc-menu ul li a:before{color: #323233;}
+     
+     
+     
+
+
+
+</style>
---
+++ src/components/RightPanle.vue
@@ -0,0 +1,58 @@
+
+<script setup lang="ts">
+    import { ref } from 'vue' 
+    const count = ref("条码追溯（老）")
+</script>
+
+
+<template>
+   <div class="sc-top">{{ count }}</div>
+   <div class="sc-content">
+        <div class="sc-left">
+
+            <div>SN二维码：<input type="text" name="snstr" id="snstr"></div>
+            <div>HU蓝标：<input type="text" name="hustr" id="hustr"></div>
+            <div>物料号： <input type="text" name="itemstr" id="itemstr"></div>
+            <div>日期：<input type="date" name="date1" id="date1">到<input type="date" name="date2" id="date2"></div>
+            <div><input type="button" value="查询"></div>
+
+        </div>
+        <div class="sc-right">22</div>
+   </div>
+</template>
+
+
+<style scoped>
+    .sc-top {
+        position: fixed;
+        top: 0; 
+        left: 10%; 
+        width: calc(90% - 17px);
+        height:2.5em; 
+        line-height:2.5em; 
+        background-color: rgb(255, 255, 255);  
+        border-bottom: 1px solid  #f5f3f3;
+        border-left: 1px solid  #d6d4d4;
+        z-index: 999;
+
+        padding-left: 2em;
+    }
+    .sc-content {    
+        margin-top: 2.5em;
+        padding: 1em;
+        overflow: auto;
+
+        display: flex;
+    }
+
+
+
+
+</style>
+
+
+
+
+
+
+
