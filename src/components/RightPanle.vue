408a58b24e005e4aa2e53c973e901589b957c8d0
guigutan <345405123@qq.com>
2025-11-20 13:43:33 +0800

    前端大体完成

--- src/components/RightPanle.vue
+++ src/components/RightPanle.vue
@@ -1,23 +1,40 @@
 
 <script setup lang="ts">
     import { ref } from 'vue' 
-    const count = ref("条码追溯（老）")
+    const projectName = ref("条码追溯（老）")
 </script>
 
 
 <template>
-   <div class="sc-top">{{ count }}</div>
+   <div class="sc-top">{{ projectName }}</div>
    <div class="sc-content">
         <div class="sc-left">
-
-            <div>SN二维码：<input type="text" name="snstr" id="snstr"></div>
-            <div>HU蓝标：<input type="text" name="hustr" id="hustr"></div>
-            <div>物料号： <input type="text" name="itemstr" id="itemstr"></div>
-            <div>日期：<input type="date" name="date1" id="date1">到<input type="date" name="date2" id="date2"></div>
-            <div><input type="button" value="查询"></div>
-
+             <table class="sc-stable">               
+                <tbody>
+                    <tr>
+                        <td>SN二维码：</td>
+                        <td><input type="text" name="snstr" id="snstr" ></td>
+                    </tr>
+                    <tr>
+                        <td>HU蓝标：</td>
+                        <td><input type="text" name="hustr" id="hustr"></td>
+                    </tr>
+                    <tr>
+                        <td>物料号：</td>
+                        <td><input type="text" name="itemstr" id="itemstr"></td>
+                    </tr>
+                    <tr>
+                        <td>日期：</td>
+                        <td><input type="date" name="date1" id="date1"> 到 <input type="date" name="date2" id="date2"></td>
+                    </tr>
+                    <tr>
+                        <td><button type="button" name="button1"  class="sc-button">清除</button></td>
+                        <td><button type="button" name="button2"  class="sc-button">查询</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        <div class="sc-right">22</div>
+        <div class="sc-right">right</div>
    </div>
 </template>
 
@@ -35,7 +52,7 @@
         border-left: 1px solid  #d6d4d4;
         z-index: 999;
 
-        padding-left: 2em;
+        padding-left: 2em; 
     }
     .sc-content {    
         margin-top: 2.5em;
@@ -44,7 +61,16 @@
 
         display: flex;
     }
-
+    .sc-stable tr td{
+        padding: 3px;
+    }
+    .sc-stable tr td input{
+        height: 20px;
+        padding:0 5px;
+    }
+    .sc-button{
+        margin-top: 2em;
+    }
 
 
 
